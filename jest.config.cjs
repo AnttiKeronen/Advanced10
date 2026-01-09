478c15534f37e7fc5e3306c499d0c45f3e494b53
AnttiKeronen <antti.keronen@student.lut.fi>
2026-01-09 15:47:09 +0200

    juu

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,16 +1,16 @@
-// jest.config.cjs
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', { useESM: true, tsconfig: './tsconfig.app.json' }]
-  },
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+
+  setupFilesAfterEnv: ["<rootDir>/src/__test__/jest.setup.ts"],
+
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    "\\.(css|less|scss)$": "identity-obj-proxy"
   },
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  reporters: ['default', 'jest-junit'],
-  roots: ['<rootDir>/src/__test__'],
-  testTimeout: 10000,
-  setupFilesAfterEnv: ['<rootDir>/src/__test__/jest.setup.ts']
+
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json"
+    }
+  }
 };
--- src/__test__/jest.setup.ts
+++ src/__test__/jest.setup.ts
@@ -1,8 +1,1 @@
-import 'cross-fetch/polyfill'; 
-
-import { TextEncoder, TextDecoder } from 'util';
-
-// @ts-ignore
-global.TextEncoder = require("util").TextEncoder;
-// @ts-ignore
-global.TextDecoder = require("util").TextDecoder;
+import '@testing-library/jest-dom';
