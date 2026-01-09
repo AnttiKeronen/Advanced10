c8057d2d5aa5c1e7f88afd88e23f3a923d6abbf4
AnttiKeronen <antti.keronen@student.lut.fi>
2026-01-09 16:19:37 +0200

    ah

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,12 +1,8 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./src/__test__/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/__test__/jest.setup.ts'],
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  },
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '\\.css$': 'identity-obj-proxy'
   }
 };
--- src/__test__/jest.setup.ts
+++ src/__test__/jest.setup.ts
@@ -1,7 +1,1 @@
-import 'cross-fetch/polyfill';
 import '@testing-library/jest-dom';
-
-import { TextEncoder, TextDecoder } from 'util';
-
-global.TextEncoder = TextEncoder as any;
-global.TextDecoder = TextDecoder as any;
